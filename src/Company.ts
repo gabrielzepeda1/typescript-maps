efc8a4d76b09611fd1cea7aa24901d5504ed109a
Gabriel Zepeda <gabrielzepeda846@gmail.com>
2024-04-02 11:02:40 -0600

    Added User and Company classes.

---
+++ src/Company.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+
+export class Company {
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.catchPhrase = faker.company.catchPhrase();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
